f8566e567cb551d737a743307930b4bdbe908b14
Alex Kamnev <aleksandrstoun73@gmail.com>
2023-10-08 17:39:49 +0600

    Initial commit

---
+++ src/main/java/com/example/twitter2/entity/Message.java
@@ -0,0 +1,33 @@
+package com.example.twitter2.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String text;
+    private String tag;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    public Message() {
+    }
+
+    public Message(String text, String tag, User user) {
+        this.author = user;
+        this.text = text;
+        this.tag = tag;
+    }
+
+    public String getAuthorName() {
+        return author != null ? author.getUsername() : "<none>";
+    }
+}
---
+++ src/main/java/com/example/twitter2/entity/Role.java
@@ -0,0 +1,12 @@
+package com.example.twitter2.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
